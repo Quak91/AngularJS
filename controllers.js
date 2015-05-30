54089ab9066937464117a4590b4d874ea12cfac1
Quak91 <piotr.quak@gmail.com>
2015-05-31 01:43:58 +0200

    przyciski do nawigacji

--- controllers.js
+++ controllers.js
@@ -1,8 +1,8 @@
 var appControllers = angular.module('AppControllers',[]);
 
 appControllers.controller('MoviesListController', ['$scope', '$routeParams', '$http', '$q', function($scope, $routeParams, $http, $q) {
-	$scope.dataTmp = $http.get('http://api.themoviedb.org/3/discover/movie?page='+$routeParams.page+'&sort_by=vote_average.desc&vote_count.gte=100&language=pl&api_key=58aa31b7945dbe8cb06c4558a0406796', {cache: false});
-	$scope.genresTmp = $http.get('http://api.themoviedb.org/3/genre/movie/list?language=pl&api_key=58aa31b7945dbe8cb06c4558a0406796', {cache: false});
+	$scope.dataTmp = $http.get('http://api.themoviedb.org/3/discover/movie?page='+$routeParams.page+'&sort_by=vote_average.desc&vote_count.gte=100&language=pl&api_key=58aa31b7945dbe8cb06c4558a0406796');
+	$scope.genresTmp = $http.get('http://api.themoviedb.org/3/genre/movie/list?language=pl&api_key=58aa31b7945dbe8cb06c4558a0406796');
 	$q.all([$scope.dataTmp, $scope.genresTmp]).then(function(values) {
 		$scope.data = values[0].data;
 		$scope.genres = values[1].data.genres;
