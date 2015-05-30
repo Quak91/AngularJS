f16425bcafd6a00a37e44d5f8de5659d45c9f97b
Quak91 <piotr.quak@gmail.com>
2015-05-30 21:31:34 +0200

    dodałem twitter-bootstrap, wstępna tabela z listą filmów

--- controllers.js
+++ controllers.js
@@ -3,7 +3,7 @@ var appControllers = angular.module('AppControllers',[]);
 appControllers.controller('MoviesListController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
 	$http.get('http://api.themoviedb.org/3/discover/movie?page='+$routeParams.page+'&sort_by=vote_average.desc&vote_count.gte=100&language=pl&api_key=58aa31b7945dbe8cb06c4558a0406796')
 		.success(function(data) {
-			$scope.movieList = data; // po odebraniu danych, lista filmów ma być dostępna w movie-list.html
+			$scope.data = data; // po odebraniu danych, lista filmów ma być dostępna w movie-list.html
 		});
 }]);
 
