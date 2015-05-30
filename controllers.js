587d5e029a62e54d29f3015a5404180293d1eedc
Quak91 <piotr.quak@gmail.com>
2015-05-30 20:23:12 +0200

    wyciąganie numeru strony oraz id filmu z adresu

--- controllers.js
+++ controllers.js
@@ -1,9 +1,11 @@
 var appControllers = angular.module('AppControllers',[]);
 
-appControllers.controller('MoviesListController', ['$scope', function($scope) {
+appControllers.controller('MoviesListController', ['$scope', '$routeParams', function($scope, $routeParams) {
 	// tu trzeba pobrać listę filmów (wyciągnąć :page), żeby była dostępna w movies-list.html
+	$scope.page = $routeParams.page;
 }]);
 
-appControllers.controller('MovieController', ['$scope', function($scope) {
+appControllers.controller('MovieController', ['$scope', '$routeParams', function($scope, $routeParams) {
 	// tu trzeba pobrać film z id= :id, żeby był dostępny w movie.html
+	$scope.id = $routeParams.id;
 }]);
