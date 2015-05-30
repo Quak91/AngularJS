7fe08c717eaa9ea7d5205a305543c53ccb186cd2
Quak91 <piotr.quak@gmail.com>
2015-05-30 20:51:17 +0200

    działa odbieranie danych z zewnętrznego API

--- controllers.js
+++ controllers.js
@@ -1,11 +1,13 @@
 var appControllers = angular.module('AppControllers',[]);
 
-appControllers.controller('MoviesListController', ['$scope', '$routeParams', function($scope, $routeParams) {
-	// tu trzeba pobrać listę filmów (wyciągnąć :page), żeby była dostępna w movies-list.html
-	$scope.page = $routeParams.page;
+appControllers.controller('MoviesListController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+	$http.get('http://api.themoviedb.org/3/discover/movie?page='+$routeParams.page+'&sort_by=vote_average.desc&vote_count.gte=100&language=pl&api_key=58aa31b7945dbe8cb06c4558a0406796')
+		.success(function(data) {
+			$scope.movieList = data; // po odebraniu danych, lista filmów ma być dostępna w movie-list.html
+		});
 }]);
 
-appControllers.controller('MovieController', ['$scope', '$routeParams', function($scope, $routeParams) {
-	// tu trzeba pobrać film z id= :id, żeby był dostępny w movie.html
-	$scope.id = $routeParams.id;
+appControllers.controller('MovieController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+	//$routeParams.id; //numer id filmu (z adresu w przeglądarce)
+	// tu trzeba pobrać dane pojedynczego filmu z id=$routeParams.id
 }]);
